e008b1153df84274839c3fbc052fe4982f503ee9
AnaoTurco <75632668+AnaoTurco@users.noreply.github.com>
2023-11-23 17:41:47 -0300

    Add files via upload

---
+++ Mob/assets/login.js
@@ -0,0 +1,11 @@
+
+
+function logar(){
+var login = document.getElementById('login').value;
+var senha = document.getElementById ('senha').value;
+
+if(login == "Brian" && senha=="1234"){
+    location.href= "index.html";  
+}
+
+}
